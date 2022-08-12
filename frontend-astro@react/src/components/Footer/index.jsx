036cb63b89da05e050ea78a739f8f7e513063be0
Chelsie L. Bangloy-Gonsalves <chelsiegonsalves@proton.me>
2022-08-11 14:59:21 -1000

    continue to work on nav icon hover effect and fix placement

--- frontend-astro@react/src/components/Footer/index.jsx
+++ frontend-astro@react/src/components/Footer/index.jsx
@@ -4,8 +4,8 @@ import Styles from "./styles.module.scss";
 function Footer() {
   return (
     <footer className={Styles.footer}>
-      &copy; {new Date().getFullYear()} Jeanine White
-      <small className={Styles.byline}>🚀 Built by Astro</small>
+      &copy; {new Date().getFullYear()} Chelsie L. Bangloy-Gonsalves
+      <small className={Styles.byline}>Blackcat-Tinkerngineers</small>
     </footer>
   );
 }
